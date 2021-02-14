c76145383c877a3a12a5fd02bae3c38710b0dd8d
Marcos Acosta <macosta1625@gmail.com>
2021-02-14 11:54:51 -0500

    Production env

--- server.js
+++ server.js
@@ -67,5 +67,16 @@ apiRouter.use('/users', users);
 apiRouter.use('/bottles', bottles);
 apiRouter.use('/session', sessions);
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
