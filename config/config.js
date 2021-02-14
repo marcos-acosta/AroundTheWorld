9bce111dd2079a808442d68f7dddfa6eac4b7ff3
Marcos Acosta <macosta1625@gmail.com>
2021-02-14 11:46:56 -0500

    Security breach

---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  SESS_NAME: 'sid',
+  SESS_SECRET: 'secret!session',
+  SESS_LIFETIME: 1000 * 60 * 60 * 2,
+  PORT: 5000,
+  NODE_ENV: 'dev'
+}
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb+srv://dbUser:dbUserPassword@cluster0.48cvp.mongodb.net/mib?retryWrites=true&w=majority",
+  secretOrKey: "secret"
+};
